d6d892d6a6e8e1a4f93da20b2ea9f2d0473e6d83
V-UNiQuE-V <panvalkarvallari@gmail.com>
2025-09-29 07:43:07 +0530

    "setup"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import OTP from "./pages/OTP";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";;
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
-  return <></>;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/password/forgot" element={<ForgotPassword/>}></Route>
+      <Route path="/otp-verification/:email" element={<OTP/>}></Route>
+      <Route path="/password/reset/:token" element={<ResetPassword/>}></Route>
+    </Routes>
+    <ToastContainer theme="dark"/>
+  </Router>
 };
 
 export default App;
