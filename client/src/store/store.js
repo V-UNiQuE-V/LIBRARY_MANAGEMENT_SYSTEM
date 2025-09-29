5dc6dea152c2025e9df43917bf8cfc83b1f47272
V-UNiQuE-V <panvalkarvallari@gmail.com>
2025-09-29 10:54:32 +0530

    registration-ui

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"
-import popupReducer from "./slices/popUpSlice"
+import popupReducer from "./slices/popupSlice";
 
 
 export const store = configureStore({
